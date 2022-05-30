66a9eefc2324da20f5ff9a0450cb3bc414fe089d
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2022-05-30 14:15:06 +0100

    fix translation

--- contao/languages/de/tl_image_size_item.php
+++ contao/languages/de/tl_image_size_item.php
@@ -10,4 +10,4 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
-$GLOBALS['TL_LANG']['tl_image_size_item']['alternative'] = ['Alternative', 'Wählre eine alternative Bild-Version die für diese Bildgröße verwendet werden soll, falls vorhanden.'];
+$GLOBALS['TL_LANG']['tl_image_size_item']['alternative'] = ['Alternative', 'Wähle eine alternative Bild-Version die für diese Bildgröße verwendet werden soll, falls vorhanden.'];
