05a46c4915b3109283b7d721dbfcf23476525c1e
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2022-10-10 15:55:49 +0200

    CS

--- contao/dca/tl_files.php
+++ contao/dca/tl_files.php
@@ -13,14 +13,6 @@ declare(strict_types=1);
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 use InspiredMinds\ContaoImageAlternatives\DataContainer\FolderDriver;
 
-/*
- * This file is part of the Contao Image Alternatives extension.
- *
- * (c) inspiredminds
- *
- * @license LGPL-3.0-or-later
- */
-
 $GLOBALS['TL_DCA']['tl_files']['config']['dataContainer'] = FolderDriver::class;
 
 $GLOBALS['TL_DCA']['tl_files']['fields']['importantParts'] = [
